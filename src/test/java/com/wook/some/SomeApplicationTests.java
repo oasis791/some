9c7f4bc863656179f612d30b057f4e1e051a8ce7
oasis791 <khwi791@naver.com>
2026-01-31 18:10:48 +0900

    Initial Commit

---
+++ src/test/java/com/wook/some/SomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wook.some;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
